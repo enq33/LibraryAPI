5c2ac3a4541d845f0c320df92eab5b8c9b1b9610
Taomi <vedyakov.savely@ya.ru>
2026-01-14 23:14:23 +0500

    add pydantic schemas for books

---
+++ schemas/books.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class SBookBase(BaseModel):
+    title: str
+    author: str
+    year: int
+    pages: int = Field(..., gt=10)
+    is_read: bool = False
+
+
+class SBookAdd(SBookBase):
+    pass
+
+
+class SBook(SBookBase):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
